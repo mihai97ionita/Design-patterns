1df8205610d91cbd8984568657ffa0a2fcbfa5ec
Mihai <59287942+mihai97ionita@users.noreply.github.com>
2020-06-08 17:55:43 +0300

    start.spring.io

---
+++ src/test/kotlin/com/mihai97ionita/patterns/PatternsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mihai97ionita.patterns
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PatternsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
